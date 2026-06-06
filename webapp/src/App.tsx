c967fbf1675e8cbae4e311ca9617981a72f9add6
copyrightnews <copyrightnews@users.noreply.github.com>
2026-06-07 00:00:25 +0600

    feat(webapp): restore AppShell + Toast composition

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,9 +1,16 @@
 import { AppRouter } from "./router";
 import { TelegramProvider } from "./components/TelegramProvider";
+import { AppShell } from "./components/AppShell";
 import { Toast } from "./components/Toast";
+import { Outlet } from "react-router-dom";
+import React from "react";
 
-function ShellWrapper({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+function ShellWrapper() {
+  return (
+    <AppShell>
+      <Outlet />
+    </AppShell>
+  );
 }
 
 export default function App() {
