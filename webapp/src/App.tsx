99ce79bfe61751c5b734486440b9e5441be7f1f9
copyrightnews <copyrightnews@users.noreply.github.com>
2026-06-06 23:51:06 +0600

    feat(webapp): TelegramProvider + theme application on mount

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,5 +1,10 @@
 import { AppRouter } from "./router";
+import { TelegramProvider } from "./components/TelegramProvider";
 
 export default function App() {
-  return <AppRouter />;
+  return (
+    <TelegramProvider>
+      <AppRouter />
+    </TelegramProvider>
+  );
 }
